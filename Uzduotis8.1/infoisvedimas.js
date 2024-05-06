c31b33d6e50a233ce02c39c89503c262fb455c14
Rengoku-hashira <blacky.zx9@gmail.com>
2024-05-06 20:05:05 +0300

    nauji css + js

---
+++ Uzduotis8.1/infoisvedimas.js
@@ -0,0 +1,27 @@
+console.log('Ernestas 24m');
+console.log('Money money money');
+console.log('lorem  \n ipsum \n dolor \n sit \n amet');
+
+console.log('***'); console.log('* *'); console.log('***');
+
+
+console.log('*');
+console.log('**');
+console.log('***');
+
+console.log('Trust \n the \n Process');
+alert('Ivyko klaida'); 
+
+
+console.log('+--------+--------+');
+console.log('| Vardas | Amzius |');
+console.log('+--------+--------+');
+console.log('| Tomas | 24      |');
+console.log('| Jonas | 26      |');
+console.log('| Juste | 25      |');
+console.log('+--------+--------+');
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('Race').innerHTML = 'Toaster';});
---
+++ uzduotis8.3/aritmet.js
@@ -0,0 +1,135 @@
+//console.log(5 + 2);
+//console.log(7+3);
+//console.log(9 - 1);
+//console.log(8 * 4);
+//console.log(10 / 3); 
+
+//console.log(5 + 3 * 2);
+//console.log(10 - 5 * (7 + 2));
+
+//let atsakymas = 8 * 2 + 4;
+//console.log(atsakymas);
+
+
+//let x = 10;
+//let y = 3;
+
+//console.log(x + y); // pridejimas
+//console.log(x - y); // atemimas
+//console.log(x * y); // daugyba
+//console.log(x / y); // dalyba
+//console.log(x % y); // liekana
+//console.log(x ** y); // laipsnis
+
+//let a = 5;
+//let b = 4;
+
+//let sum = a + b;
+//let subs = a - b;
+//let mult = a * b;
+//let div = a / b;
+
+
+
+//console.log("a: " + a);
+//console.log("b: " + b);
+//console.log("suma: " + sum);
+//console.log("atimtis: " + subs);
+//console.log("daugyba: " + mult);
+//console.log("dalyba: " + div);
+
+//let c = 5;
+//let d = 4;
+
+//console.log("c: " + c);
+//console.log("d: " + d);
+
+//console.log("suma: " + c + d);
+//console.log("suma: " + (c + d));
+
+//console.log("atimtis: " + c - d);
+//console.log("atimtis: " + (c - d));
+
+//console.log("daugyba: " + c * d);
+
+//console.log("dalyba: " + (c / d));
+
+
+//let a = 11;
+//let b = 7;
+
+//console.log(a / b);
+//console.log(a % b);
+
+
+
+//let a = 5;
+//let b = 7;
+
+//let c = a + b * 2;
+//let d = a + c;
+
+//console.log("a:", a);
+//console.log("b:", b);
+//console.log("c:", c);
+//console.log("d:", d);
+
+// 3 uzduotis
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a - b);
+console.log((a + b) / (a - b));
+
+
+// 2 uzduotis
+//let q = 11;
+//let w = 44;
+//let p = 350;
+
+//console.log(q + w + p);
+//console.log(q * w * p);
+
+
+
+// 5 uzduotis
+let v = 4;
+let n = 5;
+
+console.log(v + n);
+
+
+// 6 uzduotis
+let o = -1;
+let z = 4;
+let m = 6;
+
+console.log(o + z * 6);
+
+// 6 uzduotis
+let s = 35;
+let l = 5;
+let t = 7;
+
+console.log((s + l) % t);
+
+// 6 uzduotis
+let y = 14;
+let f = -4;
+let h = 6;
+let k = 12;
+
+console.log(y + f * h / k);
+
+// 6 uzduotis
+let d = 2;
+let u = 15;
+let g = 6;
+let q = 1;
+let w = 7;
+let p = 2;
+
+console.log(d + u / g * q - w % p);
+
+
