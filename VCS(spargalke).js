b4df9047a21436c19d230eeec2255e01420934d1
Rengoku-hashira <blacky.zx9@gmail.com>
2024-05-13 21:15:41 +0300

    uzrasine

--- VCS(spargalke).js
+++ VCS(spargalke).js
@@ -1,3 +1,6 @@
 // && (description) - and(ir) + example (x< 10 && y > 1) is true
 // || (description) - or(arba) + example (x == 5 || y == 5) is false
 // ! (description) - not(ne) + example !(x == y) is true
+
+
+
