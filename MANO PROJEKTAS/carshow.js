a2d1a457e21dbbee82efdc70b029bb720aa6524c
Rengoku-hashira <blacky.zx9@gmail.com>
2024-06-08 17:29:28 +0300

    sliders

--- MANO PROJEKTAS/carshow.js
+++ MANO PROJEKTAS/carshow.js
@@ -9,3 +9,82 @@ document.getElementById('search-icon').addEventListener('click', function() {
 
 
 
+
+
+
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  if (index >= totalSlides / 2) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = Math.floor(totalSlides / 2) - 1;
+  } else {
+    currentSlide = index;
+  }
+  const sliderWidth = document.querySelector('.slider').clientWidth;
+  const newTransform = -currentSlide * (sliderWidth / 2);
+  document.querySelector('.slides').style.transform = `translateX(${newTransform}px)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 3 seconds
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let currentSlide = 0;
+
+// const slides = document.querySelectorAll('.slide');
+// const totalSlides = slides.length;
+
+// function showSlide(index) {
+//   if (index >= totalSlides / 2) {
+//     currentSlide = 0;
+//   } else if (index < 0) {
+//     currentSlide = totalSlides / 2 - 1;
+//   } else {
+//     currentSlide = index;
+//   }
+//   const sliderWidth = document.querySelector('.slider').clientWidth;
+//   const newTransform = -currentSlide * (sliderWidth / 2);
+//   document.querySelector('.slides').style.transform = `translateX(${newTransform}px)`;
+// }
+
+// function nextSlide() {
+//   showSlide(currentSlide + 1);
+// }
+
+// function prevSlide() {
+//   showSlide(currentSlide - 1);
+// }
+
+// setInterval(nextSlide, 3000); 
